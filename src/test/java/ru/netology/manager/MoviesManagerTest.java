4669a10e663768037e433130b8e48996143fd186
Anna Yakovleva <_bel4onok_@mail.ru>
2021-10-11 21:48:16 +0300

    Optimize MoviesManagerTest

--- src/test/java/ru/netology/manager/MoviesManagerTest.java
+++ src/test/java/ru/netology/manager/MoviesManagerTest.java
@@ -1,17 +1,26 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Movie;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MoviesManagerTest {
-    @Test
-    public void shouldGetLatestWithoutLimit() {
-        MoviesManager manager = new MoviesManager();
+    private Movie movie1;
+    private Movie movie2;
+    private Movie movie3;
+
+    @BeforeEach
+    public void setUp() {
         Movie movie1 = new Movie(1, "Оно", "Ужасы", "https://www.themoviedb.org/t/p/original/kSIR8M81ldKXTpw1B7UZtttskDG.jpg");
         Movie movie2 = new Movie(2, "Оно 2", "Ужасы", "https://www.themoviedb.org/t/p/w500/rjM13nwgzt80xAUKXVp9mzRYyHp.jpg");
         Movie movie3 = new Movie(3, "Время", "Ужасы", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/m0rqyzs7IbNaXuJvLTubCG40F92.jpg");
+    }
+
+    @Test
+    public void shouldGetLatestWithoutLimit() {
+        MoviesManager manager = new MoviesManager();
         manager.add(movie1);
         manager.add(movie2);
         manager.add(movie3);
@@ -23,9 +32,6 @@ public class MoviesManagerTest {
     @Test
     public void shouldGetLatestWithLimit() {
         MoviesManager manager = new MoviesManager(2);
-        Movie movie1 = new Movie(1, "Оно", "Ужасы", "https://www.themoviedb.org/t/p/original/kSIR8M81ldKXTpw1B7UZtttskDG.jpg");
-        Movie movie2 = new Movie(2, "Оно 2", "Ужасы", "https://www.themoviedb.org/t/p/w500/rjM13nwgzt80xAUKXVp9mzRYyHp.jpg");
-        Movie movie3 = new Movie(3, "Время", "Ужасы", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/m0rqyzs7IbNaXuJvLTubCG40F92.jpg");
         manager.add(movie1);
         manager.add(movie2);
         manager.add(movie3);
